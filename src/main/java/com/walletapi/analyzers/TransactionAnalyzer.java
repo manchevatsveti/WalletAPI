c9e00ca1e0beb628a16207f8b4c4c04346aec808
Tsvetelina Mancheva <72023208+manchevatsveti@users.noreply.github.com>
2025-01-27 03:42:45 +0200

    Delete src/main/java/com/walletapi/analyzers directory

--- src/main/java/com/walletapi/analyzers/TransactionAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.walletapi.analyzers;
-
-import main.java.com.walletapi.models.Transaction;
-
-import java.util.List;
-
-public interface TransactionAnalyzer {
-    boolean analyze(Transaction transaction, List<Transaction> transactionHistory);
-}
